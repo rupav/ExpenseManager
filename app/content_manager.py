dcd2a68f3fd6725fc7afc04e2dfbcdfa816d211b
rupav jain <rupavwinchester@gmail.com>
2017-12-23 16:18:48 +0530

    removed unnecessory content

--- app/content_manager.py
+++ app/content_manager.py
@@ -1,9 +1,3 @@
-def Content():
-	TOPIC_DICT = {
-	'Basic' : [["Hello rupav","/rupav/"],["Hello Anu","/anu/"],["Hello Mom","/Mom/"],["Hello Dad","/Dad/"]]
-	}
-	return TOPIC_DICT
-
 def CategoriesText():
 	CATS = {
 	'Daily' : ['Food', 'Travel', 'Clothing', 'Entertainment', 'Online Shopping'],
