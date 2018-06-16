eae519adea9df6070448eaacd049848b7317df75
rupav jain <rupavwinchester@gmail.com>
2018-06-16 23:51:09 +0530

    Update MyApp.py

--- app/MyApp.py
+++ app/MyApp.py
@@ -63,7 +63,7 @@ def login_required(f):
 	@wraps(f)
 	def wrap(*args, **kwargs):
 		if 'logged_in' in session:
-			return f(*args,*kwargs)
+			return f(*args,**kwargs)
 		else:
 			flash('You need to login first!', "warning")
 			return redirect(url_for('login_page'))
